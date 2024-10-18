4eeee73fc598f27496e1c494e2ad481e3ae52635
tjjfvi <t6@t6.fyi>
2024-10-18 17:03:58 -0400

    fix assign op evaluation order

--- tests/tests.rs
+++ tests/tests.rs
@@ -46,6 +46,7 @@ fn tests(t: &mut DynTester) {
     test_vi(t, "tests/programs/option_party.vi", b"", ".txt");
     test_vi(t, "tests/programs/pretty_div.vi", b"", ".txt");
     test_vi(t, "tests/programs/so_random.vi", b"", ".txt");
+    test_vi(t, "tests/programs/square_case.vi", b"", ".txt");
 
     t.group("fail", |t| {
       test_vi_fail(t, "tests/programs/fail/hallo_world.vi");
