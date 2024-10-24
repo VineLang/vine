393c05a305e559826056833791f240b5084a02e4
tjjfvi <t6@t6.fyi>
2024-10-24 15:56:18 -0400

    ...

--- ivy/src/foo.rs
+++ ivy/src/foo.rs
@@ -1,9 +1,14 @@
 #![allow(unused)]
 
-use std::{collections::HashMap, mem::take};
+use std::{
+  collections::{hash_map::Entry, HashMap},
+  mem::take,
+};
 
 use ivm::ext::ExtFn;
 use slab::Slab;
+
+use crate::ast::{Net, Tree};
 
 type Wire = usize;
 type Time = usize;
@@ -31,11 +36,14 @@ impl Port {
   }
 }
 
-struct Net {
+struct FooNet {
   wires: Slab<Option<(Port, Time)>>,
   inert: Vec<(Port, Port, Time)>,
   stack: Vec<(Port, Port)>,
   clock: Time,
+  trees: HashMap<Wire, Tree>,
+  vars: HashMap<String, Wire>,
+  next_var: usize,
 }
 
 macro_rules! sym {
@@ -47,7 +55,7 @@ macro_rules! sym {
   };
 }
 
-impl Net {
+impl FooNet {
   fn link(&mut self, a: Port, b: Port) {
     match (a, b) {
       sym!(Port::Wire(a), p) => {
@@ -90,7 +98,39 @@ impl Net {
     self.wires.insert(None)
   }
 
-  fn finish(&mut self) {
+  fn insert_net(&mut self, net: &Net, root: Wire) {}
+
+  fn insert_tree_wire(&mut self, tree: &Tree) -> Wire {
+    todo!()
+  }
+
+  fn insert_tree_port(&mut self, tree: &Tree) -> Port {
+    match tree {
+      Tree::Erase => Port::Erase,
+      Tree::U32(n) => Port::U32(*n),
+      Tree::F32(n) => Port::F32(*n),
+      Tree::Global(g) => Port::Global(g.clone()),
+      Tree::Comb(l, a, b) => {
+        let a = self.insert_tree_wire(a);
+        let b = self.insert_tree_wire(b);
+        Port::Comb(l.clone(), [a, b])
+      }
+      Tree::ExtFn(f, a, b) => {
+        let a = self.insert_tree_wire(a);
+        let b = self.insert_tree_wire(b);
+        Port::ExtFn(*f, [a, b])
+      }
+      Tree::Branch(a, b, c) => {
+        let a = self.insert_tree_wire(a);
+        let b = self.insert_tree_wire(b);
+        let c = self.insert_tree_wire(c);
+        Port::Branch([a, b, c])
+      }
+      Tree::Var(_) => unreachable!(),
+    }
+  }
+
+  fn finish(&mut self) -> Net {
     self.inert.extend(
       take(&mut self.wires)
         .into_iter()
@@ -103,6 +143,49 @@ impl Net {
     );
 
     self.inert.sort_unstable_by_key(|x| x.2);
+
+    let mut pairs = Vec::new();
+    for (a, b, _) in take(&mut self.inert) {
+      let a = self.read_port(a);
+      let b = self.read_port(b);
+      pairs.push((a, b));
+    }
+    let root = self.read_wire(0);
+
+    Net { root, pairs }
+  }
+
+  fn read_port(&mut self, p: Port) -> Tree {
+    match p {
+      Port::Erase => Tree::Erase,
+      Port::U32(n) => Tree::U32(n),
+      Port::F32(n) => Tree::F32(n),
+      Port::Global(g) => Tree::Global(g),
+      Port::Comb(l, [a, b]) => {
+        Tree::Comb(l, Box::new(self.read_wire(a)), Box::new(self.read_wire(b)))
+      }
+      Port::ExtFn(f, [a, b]) => {
+        Tree::ExtFn(f, Box::new(self.read_wire(a)), Box::new(self.read_wire(b)))
+      }
+      Port::Branch([a, b, c]) => Tree::Branch(
+        Box::new(self.read_wire(a)),
+        Box::new(self.read_wire(b)),
+        Box::new(self.read_wire(c)),
+      ),
+      Port::Wire(w) => self.read_wire(w),
+    }
+  }
+
+  fn read_wire(&mut self, w: Wire) -> Tree {
+    match self.trees.entry(w) {
+      Entry::Occupied(e) => e.remove(),
+      Entry::Vacant(e) => {
+        let v = Tree::Var(format!("n{}", self.next_var));
+        self.next_var += 1;
+        e.insert(v.clone());
+        v
+      }
+    }
   }
 
   fn interact(&mut self, a: Port, b: Port) {
@@ -112,6 +195,10 @@ impl Net {
         for &p in x.aux() {
           self.link_wire(p, Port::Erase);
         }
+      }
+      (Port::Comb(l1, [a, b]), Port::Comb(l2, [x, y])) if l1 == l2 => {
+        self.link_wire_wire(a, x);
+        self.link_wire_wire(b, y);
       }
       (a, b) => {
         self.inert.push((a, b, self.clock));
