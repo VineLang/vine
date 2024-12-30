a51dbc129930c980562cff4010619469c5b32f36
tjjfvi <t6@t6.fyi>
2024-12-30 17:37:42 -0500

    ...

--- ivy/src/optimize.rs
+++ ivy/src/optimize.rs
@@ -16,14 +16,13 @@ pub struct Optimizer {
 
 impl Optimizer {
   pub fn optimize(&mut self, nets: &mut Nets) {
-    // prune(nets);
-    for (name, net) in nets.iter_mut() {
-      // dbg!(name);
+    prune(nets);
+    for (_, net) in nets.iter_mut() {
       self.inline_vars.apply(net);
       net.eta_reduce();
       self.inline_vars.apply(net);
     }
     inline_globals(nets);
-    // prune(nets);
+    prune(nets);
   }
 }
