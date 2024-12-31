97f8cb7591f3eec9cabac1e4e04b314393d115d5
tjjfvi <t6@t6.fyi>
2024-12-31 12:36:09 -0500

    remove vestigial code from usage

--- vine/src/analyzer/usage.rs
+++ vine/src/analyzer/usage.rs
@@ -58,20 +58,18 @@ const JOIN: [[Usage; 11]; 11] = [
 
 const INVERSE: [Usage; 11] = [N, E, M, T, S, H, G, W, R, P, Z];
 
-const TOP: [Usage; 10] = [N, E, G, E, G, G, N, W, W, G];
-const BOT: [Usage; 10] = [N, E, H, H, E, N, H, W, H, W];
-
-const EFFECT: [[Usage; 10]; 10] = [
-  [N, N, N, N, N, N, N, N, N, N],
-  [N, E, E, E, E, E, E, E, E, E],
-  [N, E, M, S, T, G, H, M, M, M],
-  [N, E, T, E, T, T, E, E, E, T],
-  [N, E, S, S, E, E, S, E, S, E],
-  [N, E, S, S, E, N, N, N, N, N],
-  [N, E, T, E, T, N, N, N, N, N],
-  [N, E, M, E, E, N, N, N, N, N],
-  [N, E, M, E, T, N, N, N, N, N],
-  [N, E, M, S, E, N, N, N, N, N],
+const EFFECT: [[Usage; 11]; 11] = [
+  [N, N, N, N, N, N, N, N, N, N, Z],
+  [N, E, E, E, E, E, E, E, E, E, Z],
+  [N, E, M, S, T, G, H, M, M, M, Z],
+  [N, E, T, E, T, T, E, E, E, T, Z],
+  [N, E, S, S, E, E, S, E, S, E, Z],
+  [N, E, S, S, E, N, N, N, N, N, Z],
+  [N, E, T, E, T, N, N, N, N, N, Z],
+  [N, E, M, E, E, N, N, N, N, N, Z],
+  [N, E, M, E, T, N, N, N, N, N, Z],
+  [N, E, M, S, E, N, N, N, N, N, Z],
+  [Z, Z, Z, Z, Z, Z, Z, Z, Z, Z, Z],
 ];
 
 impl Usage {
@@ -87,14 +85,6 @@ impl Usage {
     INVERSE[self as usize]
   }
 
-  pub fn top(self: Usage) -> Usage {
-    TOP[self as usize]
-  }
-
-  pub fn bot(self: Usage) -> Usage {
-    BOT[self as usize]
-  }
-
   pub fn effect(self: Usage, other: Usage) -> Usage {
     EFFECT[self as usize][other as usize]
   }
@@ -104,20 +94,8 @@ impl Usage {
 mod tests {
   use super::*;
 
-  fn join<const L: usize>(x: [Usage; L]) -> Usage {
-    x.into_iter().fold(N, Usage::join)
-  }
-
   fn usage() -> impl Iterator<Item = Usage> {
-    [N, E, M, S, T, G, H, W, P, R].into_iter()
-  }
-
-  fn tops() -> impl Iterator<Item = Usage> {
-    [N, E, G, W].into_iter()
-  }
-
-  fn bots() -> impl Iterator<Item = Usage> {
-    [N, E, H, W].into_iter()
+    [N, E, M, S, T, G, H, W, P, R, Z].into_iter()
   }
 
   #[test]
@@ -183,7 +161,6 @@ mod tests {
   fn inverse() {
     for a in usage() {
       assert_eq!(a.inverse().inverse(), a);
-      assert_eq!(a.top().inverse(), a.inverse().bot());
       for b in usage() {
         assert_eq!(a.union(b).inverse(), a.inverse().union(b.inverse()));
         assert_eq!(a.join(b).inverse(), b.inverse().join(a.inverse()));
@@ -192,83 +169,12 @@ mod tests {
   }
 
   #[test]
-  fn sandwich() {
+  fn effect() {
     for a in usage() {
       for b in usage() {
-        for c in usage() {
-          assert_eq!(join([a, b, a, c, a]), join([a, b, c, a]));
-          assert_eq!(join([a, b, a, c, b]), join([a, b, c, b]));
-        }
-      }
-    }
-  }
-
-  #[test]
-  fn topper() {
-    for a in usage() {
-      for b in usage() {
-        assert_eq!(a.top().union(b).top(), a.union(b).top());
-        assert_eq!(a.top().join(b).top(), a.join(b).top());
-        assert_eq!(a.join(b.top()).top(), a.join(b).top());
-      }
-    }
-  }
-
-  #[test]
-  fn collapse() {
-    for a in usage() {
-      for b in usage() {
-        for c in usage() {
-          for x in usage() {
-            assert_eq!(join([a, x, b, x, c]), join([a.top(), x, a, b, c, x, c.bot()]),)
-          }
-        }
-      }
-    }
-    for a in tops() {
-      for b in usage() {
-        for c in bots() {
-          for p in tops() {
-            for q in usage() {
-              for r in bots() {
-                let i = a.union(p);
-                let j = join([a, b, c]).union(join([p, q, r]));
-                let k = c.union(r);
-                for x in usage() {
-                  assert_eq!(
-                    join([a, x, b, x, c]).union(join([p, x, q, x, r])),
-                    join([i, x, j, x, k])
-                  )
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
-  #[test]
-  fn fixed() {
-    for a in usage() {
-      for b in usage() {
-        for c in usage() {
-          for d in usage() {
-            let f = |x| join([a, x, b, x, c]).union(d);
-            assert_eq!(f(N), f(f(N)));
-          }
-        }
-      }
-    }
-  }
-
-  #[test]
-  fn circle() {
-    for a in usage() {
-      for b in usage() {
-        let ab = EFFECT[a as usize][b as usize];
-        let ba = EFFECT[b as usize][a as usize];
-        assert!((ab.top() == G) == (ba.bot() == H));
+        let ab = a.effect(b);
+        let ba = b.effect(a);
+        assert!(matches!(ab, G | T | M) == matches!(ba, H | S | M));
         assert!(ab.inverse() == EFFECT[a.inverse() as usize][b.inverse() as usize]);
       }
     }
