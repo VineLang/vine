79b5abb887795ec72713a7ed4333dd4ba2495d32
tjjfvi <t6@t6.fyi>
2025-03-14 17:21:05 -0400

    test

--- tests/tests.rs
+++ tests/tests.rs
@@ -95,6 +95,7 @@ fn tests(t: &mut DynTester) {
     t.group("repl", |t| {
       test_vi_repl(t, "tests/programs/repl/advanced_repl.vi");
       test_vi_repl(t, "tests/programs/repl/basic_repl.vi");
+      test_vi_repl(t, "tests/programs/repl/f32_to_string.vi");
       test_vi_repl(t, "tests/programs/repl/misc.vi");
       test_vi_repl(t, "tests/programs/repl/objects.vi");
       test_vi_repl(t, "tests/programs/repl/randomness.vi");
