bafe5b79c4877ed7d673a7165ead19899d753bb1
T6 <t6@t6.fyi>
2025-10-16 15:45:52 -0400

    fix idempotency issue when formatting file with leading comment

--- tests/tests.rs
+++ tests/tests.rs
@@ -102,6 +102,7 @@ fn tests(t: &mut DynTester) {
     }
 
     t.group("fmt", |t| {
+      test_vi_fmt(t, "tests/programs/fmt/comment.vi");
       test_vi_fmt(t, "tests/programs/fmt/objects.vi");
       test_vi_fmt(t, "tests/programs/fmt/uses.vi");
     });
