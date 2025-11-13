2dc97a9f08e4244def655fcaee019f8cfd9f992e
T6 <t6@t6.fyi>
2025-11-13 18:50:49 -0500

    improve handling of keywords

--- lsp/tree-sitter-vine/queries/highlights.scm
+++ lsp/tree-sitter-vine/queries/highlights.scm
@@ -1,34 +1,33 @@
 
-[
-  "pub"
-  "mod"
-  "use"
-  "as"
-  "fn"
-  "struct"
-  "enum"
-  "type"
-  "inline_ivy!"
-  "trait"
-  "impl"
-  "match"
-  "let"
-  "const"
-  "in"
-  "is"
-  "return"
-  "do"
-  "loop"
-  "while"
-  "for"
-  "break"
-  "continue"
-  "if"
-  "when"
-  "else"
-  "true"
-  "false"
-] @keyword
+((ident) @keyword (#eq? @keyword "pub"))      "pub"      @keyword
+((ident) @keyword (#eq? @keyword "mod"))      "mod"      @keyword
+((ident) @keyword (#eq? @keyword "use"))      "use"      @keyword
+((ident) @keyword (#eq? @keyword "as"))       "as"       @keyword
+((ident) @keyword (#eq? @keyword "fn"))       "fn"       @keyword
+((ident) @keyword (#eq? @keyword "struct"))   "struct"   @keyword
+((ident) @keyword (#eq? @keyword "enum"))     "enum"     @keyword
+((ident) @keyword (#eq? @keyword "type"))     "type"     @keyword
+((ident) @keyword (#eq? @keyword "trait"))    "trait"    @keyword
+((ident) @keyword (#eq? @keyword "impl"))     "impl"     @keyword
+((ident) @keyword (#eq? @keyword "match"))    "match"    @keyword
+((ident) @keyword (#eq? @keyword "let"))      "let"      @keyword
+((ident) @keyword (#eq? @keyword "const"))    "const"    @keyword
+((ident) @keyword (#eq? @keyword "in"))       "in"       @keyword
+((ident) @keyword (#eq? @keyword "is"))       "is"       @keyword
+((ident) @keyword (#eq? @keyword "return"))   "return"   @keyword
+((ident) @keyword (#eq? @keyword "do"))       "do"       @keyword
+((ident) @keyword (#eq? @keyword "loop"))     "loop"     @keyword
+((ident) @keyword (#eq? @keyword "while"))    "while"    @keyword
+((ident) @keyword (#eq? @keyword "for"))      "for"      @keyword
+((ident) @keyword (#eq? @keyword "break"))    "break"    @keyword
+((ident) @keyword (#eq? @keyword "continue")) "continue" @keyword
+((ident) @keyword (#eq? @keyword "if"))       "if"       @keyword
+((ident) @keyword (#eq? @keyword "when"))     "when"     @keyword
+((ident) @keyword (#eq? @keyword "else"))     "else"     @keyword
+((ident) @keyword (#eq? @keyword "true"))     "true"     @keyword
+((ident) @keyword (#eq? @keyword "false"))    "false"    @keyword
+
+"inline_ivy!" @keyword
 
 [
   "."
