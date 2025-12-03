f2def3ab31de319643c3f6258440e70ef141aa2e
Enrico Zandomeni Borba <enricozb@gmail.com>
2025-12-03 12:33:07 +0100

    initial impl of Object auto-impl with key types

--- vine/src/features/object.rs
+++ vine/src/features/object.rs
@@ -2,7 +2,7 @@ use crate::{
   components::{finder::Finder, synthesizer::SyntheticImpl},
   structures::{
     tir::TirImpl,
-    types::{Type, TypeCtx, TypeKind, Types},
+    types::{Inverted, Type, TypeCtx, TypeKind, Types},
   },
 };
 
@@ -13,22 +13,42 @@ impl Finder<'_> {
     types: &Types,
     found: &mut Vec<TypeCtx<TirImpl>>,
   ) {
-    // TODO(enricozb): implement the synthetic impl construction to handle
-    // various omissions of the four parameters of the Object trait:
-    // [Object, Key, Init, Rest].
+    let [object_ty, key_ty, init_ty, rest_ty] = *type_params else { unreachable!() };
 
-    if let Some((inv, TypeKind::Object(entries))) = types.kind(type_params[0]) {
-      let mut iter = entries.iter();
-      if let Some((key, &init)) = iter.next() {
-        let init = init.invert_if(inv);
-        let rest = iter.map(|(k, &t)| (k.clone(), t.invert_if(inv))).collect();
-        let mut types = types.clone();
-        let rest = types.new(TypeKind::Object(rest));
-        if types.unify(init, type_params[2]).and(types.unify(rest, type_params[3])).is_success() {
-          let impl_ = TirImpl::Synthetic(SyntheticImpl::Object(key.clone(), entries.len()));
-          found.push(TypeCtx { types, inner: impl_ });
+    match types.kind(object_ty) {
+      Some((inv, TypeKind::Object(entries))) => {
+        let mut iter = entries.iter();
+        if let Some((key, &init)) = iter.next() {
+          let init = init.invert_if(inv);
+          let rest = iter.map(|(k, &t)| (k.clone(), t.invert_if(inv))).collect();
+          let mut types = types.clone();
+          let rest = types.new(TypeKind::Object(rest));
+          if types.unify(init, init_ty).and(types.unify(rest, rest_ty)).is_success() {
+            let impl_ = TirImpl::Synthetic(SyntheticImpl::Object(key.clone(), entries.len()));
+            found.push(TypeCtx { types, inner: impl_ });
+          }
         }
       }
+      None => {
+        if let Some((Inverted(false) /* ??? */, TypeKind::Key(key))) = types.kind(key_ty)
+          && let Some((inv, TypeKind::Object(rest_entries))) = types.kind(rest_ty)
+          && !rest_entries.contains_key(key)
+        {
+          let object_entries = rest_entries
+            .iter()
+            .map(|(k, &t)| (k.clone(), t.invert_if(inv)))
+            .chain(vec![(key.clone(), init_ty)])
+            .collect();
+          let mut types = types.clone();
+          let object = types.new(TypeKind::Object(object_entries));
+          if types.unify(object, object_ty).is_success() {
+            let impl_ =
+              TirImpl::Synthetic(SyntheticImpl::Object(key.clone(), rest_entries.len() + 1));
+            found.push(TypeCtx { types, inner: impl_ });
+          }
+        }
+      }
+      _ => {}
     }
   }
 }
