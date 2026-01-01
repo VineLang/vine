1c70b1f6cbb76844e30f380af23f7d1cdd9116fa
Enrico Zandomeni Borba <enricozb@gmail.com>
2026-01-01 11:16:16 -0300

    remove div check in N64.vi, test div_by_zero in debug

--- tests/tests.rs
+++ tests/tests.rs
@@ -59,7 +59,7 @@ fn tests(t: &mut DynTester) {
     test_vi(t, "tests/programs/cond_diverge.vi", b"", ".txt", true, false, false);
     test_vi(t, "tests/programs/cubes.vi", b"", ".txt", true, false, false);
     test_vi(t, "tests/programs/cyclist.vi", b"", ".txt", true, false, false);
-    test_vi(t, "tests/programs/div_by_zero.vi", b"", ".txt", false, false, true);
+    test_vi(t, "tests/programs/div_by_zero.vi", b"", ".txt", false, true, true);
     test_vi(t, "tests/programs/f32_roundabout.vi", b"", ".txt", false, false, false);
     test_vi(t, "tests/programs/final_countdown.vi", b"", ".txt", true, false, false);
     test_vi(t, "tests/programs/find_primes.vi", b"", ".txt", true, false, false);
