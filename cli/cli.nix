0f632cad1800bea7db488bfca48b6f3831f89881
T6 <t6@t6.fyi>
2026-01-18 10:22:11 -0500

    refactor nix

---
+++ cli/cli.nix
@@ -0,0 +1,66 @@
+{
+  pkgs,
+  craneLib,
+}:
+let
+
+  self.packages.ivy = craneLib.buildPackage {
+    pname = "ivy";
+    version = "0.0.0";
+    src = pkgs.lib.fileset.toSource {
+      root = ./..;
+      fileset = pkgs.lib.fileset.unions [
+        ../Cargo.toml
+        ../util
+        ../ivm
+        ../ivy
+        ../cli
+        ../vine/Cargo.toml
+        ../lsp/Cargo.toml
+        ../tests/Cargo.toml
+      ];
+    };
+    cargoLock = ../Cargo.lock;
+    preBuild = ''
+      mkdir -p vine/src lsp/src
+      touch vine/src/lib.rs lsp/src/lib.rs
+    '';
+    cargoExtraArgs = "--no-default-features --bin ivy";
+    doCheck = false;
+  };
+
+  self.packages.vine-no-root = craneLib.buildPackage {
+    pname = "vine";
+    version = "0.0.0";
+    src = pkgs.lib.fileset.toSource {
+      root = ./..;
+      fileset = pkgs.lib.fileset.unions [
+        ../Cargo.toml
+        ../util
+        ../ivm
+        ../ivy
+        ../vine/Cargo.toml
+        ../vine/src
+        ../lsp
+        ../cli
+        ../tests/Cargo.toml
+      ];
+    };
+    cargoLock = ../Cargo.lock;
+    outputHashes = {
+      "git+https://github.com/tjjfvi/class?rev=99738e6#99738e67dd8fb3b97d65e6fc59b92f04c11519a4" =
+        "sha256-ye8DqeDRXsNpTWpGGlvWxSSc1AiXOLud99dHpB/VhZg=";
+    };
+    cargoExtraArgs = "--bin vine";
+    VINE_ROOT_PATH = "../lib/root";
+    doCheck = false;
+  };
+
+  self.packages.vine = pkgs.runCommand "vine" { } ''
+    mkdir $out
+    cp -r ${self.packages.vine-no-root}/* $out
+    mkdir $out/lib
+    cp -r ${../root} $out/lib/root
+  '';
+in
+self
--- docs/docs.nix
+++ docs/docs.nix
@@ -6,10 +6,10 @@
   pkgs,
   system,
   grammars,
-  vineNoRoot,
+  cli,
 }:
 let
-  typsitter-langs = pkgs.callPackage typsitter.lib.typsitterMkDerivation {
+  self.packages.typsitter-langs = pkgs.callPackage typsitter.lib.typsitterMkDerivation {
     typsitterGrammars = {
       inherit (typsitter.lib.typsitterDefaultGrammars pkgs)
         bash
@@ -25,17 +25,14 @@ let
 
   hyptypLib = hyptyp.lib.${system} typixLib;
 
-  docCmd = vine: out: "${vine} doc --debug --no-root root ${out}";
+  docCmd = out: "${cli.packages.vine-no-root}/bin/vine doc --debug --no-root root ${out}";
   rootDocs = pkgs.stdenvNoCC.mkDerivation {
     name = "root-docs";
     src = pkgs.lib.fileset.toSource {
       root = ../.;
       fileset = ../root;
     };
-    nativeBuildInputs = [
-      vineNoRoot
-    ];
-    buildPhase = docCmd "vine" "$out";
+    buildPhase = docCmd "$out";
   };
 
   commonArgs = {
@@ -57,7 +54,7 @@ let
       }
       {
         dest = "docs/deps/typsitter-langs/";
-        src = typsitter-langs;
+        src = self.packages.typsitter-langs;
       }
       {
         dest = "docs/deps/hyptyp";
@@ -80,28 +77,23 @@ let
       }
     ];
   };
-in
-{
-  packages = {
-    inherit typsitter-langs;
-    docs = hyptypLib.buildHyptypProject buildArgs;
-    docs-pdf = typixLib.buildTypstProject buildArgs;
+
+  self.packages.docs = hyptypLib.buildHyptypProject buildArgs;
+  self.apps.docs = flake-utils.lib.mkApp {
+    drv =
+      let
+        typstWatcher = hyptypLib.watchHyptypProject watchArgs;
+      in
+      pkgs.writeShellScriptBin "watch-docs" ''
+        ${docCmd "docs"}
+        ${pkgs.watchexec}/bin/watchexec -qw root "${docCmd "docs"}" &
+        ${typstWatcher}/bin/${typstWatcher.name}
+      '';
   };
 
-  apps = {
-    docs = flake-utils.lib.mkApp {
-      drv =
-        let
-          typstWatcher = hyptypLib.watchHyptypProject watchArgs;
-        in
-        pkgs.writeShellScriptBin "watch-docs" ''
-          ${docCmd "${vineNoRoot}/bin/vine" "docs"}
-          ${pkgs.watchexec}/bin/watchexec -qw root "${docCmd "${vineNoRoot}/bin/vine" "docs"}" &
-          ${typstWatcher}/bin/${typstWatcher.name}
-        '';
-    };
-    docs-pdf = flake-utils.lib.mkApp {
-      drv = hyptypLib.watchHyptypProject watchArgs;
-    };
+  self.packages.docs-pdf = typixLib.buildTypstProject buildArgs;
+  self.apps.docs-pdf = flake-utils.lib.mkApp {
+    drv = hyptypLib.watchHyptypProject watchArgs;
   };
-}
+in
+self
